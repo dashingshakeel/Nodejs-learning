7b4a6dbfa5dd633e30237d434c6e9f09df90108d
dashingshakeel <shkeelrauf@gmail.com>
2016-10-04 23:36:00 +0500

    Constructor function and modules

---
+++ cart2.js
@@ -0,0 +1,15 @@
+function Cart(){
+  this.items=[];
+}
+Cart.prototype.addItem=function(name,price){
+  this.items.push({
+    name: name,
+    price: price
+  });
+};
+Cart.prototype.total= function(){
+  return this.items.reduce(function(a,b){
+    return a + b.price;
+  }, 0);
+};
+module.exports = Cart;
